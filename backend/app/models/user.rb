4acef716e40e3bf5bce98078625f4e670e451ce1
ishii hyuga <hyu825@gmail.com>
2025-03-23 14:43:02 +0900

    add: ユーザーのバリデーションを追加

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-devise :database_authenticatable,
-        :registerable,
-        :recoverable,
-        :rememberable
-include DeviseTokenAuth::Concerns::User
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable
+  include DeviseTokenAuth::Concerns::User
+
+  validates :user_name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6, maximum: 13 }
 end

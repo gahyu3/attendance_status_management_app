458b2b948d7f2461fd3cfd05e4aac7b767a4abb2
ishii hyuga <hyu825@gmail.com>
2025-03-28 00:08:50 +0900

    add: ログインフォームの実装

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
 
   validates :user_name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { minimum: 6, maximum: 13 }
 end
--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -11,17 +11,20 @@
       <div class="text-center mb-4">
         <h1>ログイン</h1>
       </div>
-        <v-form>
+        <v-form @submit.prevent="submitForm">
           <v-text-field
+            v-model="form.email"
             label="メールアドレス"
             placeholder="johndoe@gmail.com"
             type="email"
           ></v-text-field>
           <v-text-field
+            v-model="form.password"
             label="パスワード"
             placeholder="●●●●●●●"
             type="password"
           ></v-text-field>
+          <div v-if="errorMessage" class="text-red mb-2">メールアドレスかパスワードが違います</div>
 
           <v-layout>
             <v-btn
@@ -38,3 +41,29 @@
     </v-container>
   </v-main>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+const config = useRuntimeConfig()
+
+const form = ref({
+  email: "",
+  password: ""
+})
+
+const response = ref(null)
+const errorMessage = ref("")
+
+const submitForm = async () => {
+  try {
+    const res = await $fetch(`${config.public.apiBase}/api/v1/sign_in`, {
+      method: "POST",
+      body: form.value
+    })
+    response.value = res
+    errorMessage.value= ""
+  } catch (error) {
+    errorMessage.value = error.data.errors.join(', ')
+  }
+}
+</script>

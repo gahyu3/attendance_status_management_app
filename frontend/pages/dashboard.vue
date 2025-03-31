4a9b24d2f8dbee41b81041d3e4098e927c392054
ishii hyuga <hyu825@gmail.com>
2025-03-31 20:18:39 +0900

    add: ログアウト機能を実装

--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -1,11 +1,44 @@
 <template>
   <p>ログイン成功</p>
+  <v-btn @click="logout">ログアウト</v-btn>
 </template>
 
 <script setup>
+const config = useRuntimeConfig()
+
 definePageMeta({
   middleware: [
-    'auth',
+    "auth",
   ],
 });
+
+const accessToken = useCookie("access-token")
+const client = useCookie("client")
+const uid = useCookie("uid")
+
+const logout = async () => {
+  if (!accessToken.value || !client.value || !uid.value) {
+    console.log("ログインしていません")
+    return
+  }
+
+  const response = await fetch(`${config.public.apiBase}/api/v1/sign_out`, {
+    method: "DELETE",
+    headers: {
+      "access-token": accessToken.value,
+      "client": client.value,
+      "uid": uid.value
+    }
+  })
+
+  if (response.ok) {
+    // クッキーを削除してログアウト
+    accessToken.value = null
+    client.value = null
+    uid.value = null
+    return navigateTo("/login")
+  } else {
+    console.error("ログアウト失敗", await response.json())
+  }
+}
 </script>

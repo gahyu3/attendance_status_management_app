c046ff681321fcc5ea486fafd50d5fe360db420b
ishii hyuga <hyu825@gmail.com>
2025-04-02 18:49:17 +0900

    add: ログインユーザーを獲得

--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -3,10 +3,11 @@
       <v-app-bar-title>
         Application
       </v-app-bar-title>
+      {{ currentUser.user_name }}
       <v-btn @click="logout">ログアウト</v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer :width="300">
+    <v-navigation-drawer permanent>
       <v-list-item title="Group"></v-list-item>
       <v-divider></v-divider>
       <v-list-item link title="List Item 1"></v-list-item>
@@ -20,6 +21,7 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
 const config = useRuntimeConfig()
 
 definePageMeta({
@@ -57,4 +59,34 @@ const logout = async () => {
     console.error("ログアウト失敗", await response.json())
   }
 }
+
+const currentUser = ref({});
+
+// マウント時にログインユーザーを獲得
+onMounted(() => {
+  currentUserFetch();
+})
+
+async function currentUserFetch() {
+  try {
+    const response = await fetch(`${config.public.apiBase}/api/v1/validate_token`, {
+      headers: {
+        "access-token": accessToken.value,
+        "client": client.value,
+        "uid": uid.value
+      }
+    });
+
+    if (response.ok) {
+      const res = await response.json();
+      currentUser.value = res.data
+    } else {
+      const errorRes = await response.json();
+      console.error("ユーザー獲得失敗", errorRes);
+    }
+  } catch (error) {
+    console.error("エラー発生:", error);
+  }
+}
+
 </script>

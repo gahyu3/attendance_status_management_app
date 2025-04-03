f062b0aeb0364fd8b97dcd7faefbf935825b557b
ishii hyuga <hyu825@gmail.com>
2025-04-04 00:09:00 +0900

    update: ヘッダーをcomponentsフォルダに切り分け

---
+++ frontend/components/AppHeader.vue
@@ -0,0 +1,84 @@
+<template>
+    <v-app-bar color="primary" height="100">
+      <v-app-bar-title>
+        Application
+      </v-app-bar-title>
+    <template v-slot:append>
+      <v-icon icon="mdi-account-circle" size="x-large" start />
+      {{ currentUser.user_name }}
+      <v-btn @click="logout">ログアウト</v-btn>
+    </template>
+  </v-app-bar>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+const config = useRuntimeConfig()
+
+definePageMeta({
+  middleware: [
+    "auth",
+  ],
+});
+
+const accessToken = useCookie("access-token")
+const client = useCookie("client")
+const uid = useCookie("uid")
+
+const logout = async () => {
+  if (!accessToken.value || !client.value || !uid.value) {
+    console.log("ログインしていません")
+    return
+  }
+
+  const response = await fetch(`${config.public.apiBase}/api/v1/sign_out`, {
+    method: "DELETE",
+    headers: {
+      "access-token": accessToken.value,
+      "client": client.value,
+      "uid": uid.value
+    }
+  })
+
+  if (response.ok) {
+    // クッキーを削除してログアウト
+    accessToken.value = null
+    client.value = null
+    uid.value = null
+    return navigateTo("/login")
+  } else {
+    console.error("ログアウト失敗", await response.json())
+  }
+}
+
+const currentUser = ref({});
+
+// マウント時にログインユーザーを獲得
+onMounted(() => {
+  currentUserFetch();
+})
+
+async function currentUserFetch() {
+  try {
+    const response = await fetch(`${config.public.apiBase}/api/v1/validate_token`, {
+      headers: {
+        "access-token": accessToken.value,
+        "client": client.value,
+        "uid": uid.value
+      }
+    });
+
+    if (response.ok) {
+      const res = await response.json();
+      currentUser.value = res.data
+    } else {
+      const errorRes = await response.json();
+      console.error("ユーザー獲得失敗", errorRes);
+    }
+  } catch (error) {
+    console.error("エラー発生:", error);
+  }
+}
+
+</script>
+
--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -1,95 +1,16 @@
 <template>
-  <v-app-bar color="primary" height="100">
-      <v-app-bar-title>
-        Application
-      </v-app-bar-title>
-    <template v-slot:append>
-      <v-icon icon="mdi-account-circle" size="x-large" start />
-      {{ currentUser.user_name }}
-      <v-btn @click="logout">ログアウト</v-btn>
-    </template>
-  </v-app-bar>
 
-    <v-navigation-drawer permanent>
-      <v-list-item title="Group"></v-list-item>
-      <v-divider></v-divider>
-      <v-list-item link title="List Item 1"></v-list-item>
-      <v-list-item link title="List Item 2"></v-list-item>
-      <v-list-item link title="List Item 3"></v-list-item>
-    </v-navigation-drawer>
+  <AppHeader />
 
-    <v-main>
-    </v-main>
+  <v-navigation-drawer permanent>
+    <v-list-item title="Group"></v-list-item>
+    <v-divider></v-divider>
+    <v-list-item link title="List Item 1"></v-list-item>
+    <v-list-item link title="List Item 2"></v-list-item>
+    <v-list-item link title="List Item 3"></v-list-item>
+  </v-navigation-drawer>
+
+  <v-main>
+  </v-main>
 
 </template>
-
-<script setup>
-import { ref, onMounted } from 'vue';
-const config = useRuntimeConfig()
-
-definePageMeta({
-  middleware: [
-    "auth",
-  ],
-});
-
-const accessToken = useCookie("access-token")
-const client = useCookie("client")
-const uid = useCookie("uid")
-
-const logout = async () => {
-  if (!accessToken.value || !client.value || !uid.value) {
-    console.log("ログインしていません")
-    return
-  }
-
-  const response = await fetch(`${config.public.apiBase}/api/v1/sign_out`, {
-    method: "DELETE",
-    headers: {
-      "access-token": accessToken.value,
-      "client": client.value,
-      "uid": uid.value
-    }
-  })
-
-  if (response.ok) {
-    // クッキーを削除してログアウト
-    accessToken.value = null
-    client.value = null
-    uid.value = null
-    return navigateTo("/login")
-  } else {
-    console.error("ログアウト失敗", await response.json())
-  }
-}
-
-const currentUser = ref({});
-
-// マウント時にログインユーザーを獲得
-onMounted(() => {
-  currentUserFetch();
-})
-
-async function currentUserFetch() {
-  try {
-    const response = await fetch(`${config.public.apiBase}/api/v1/validate_token`, {
-      headers: {
-        "access-token": accessToken.value,
-        "client": client.value,
-        "uid": uid.value
-      }
-    });
-
-    if (response.ok) {
-      const res = await response.json();
-      currentUser.value = res.data
-    } else {
-      const errorRes = await response.json();
-      console.error("ユーザー獲得失敗", errorRes);
-    }
-  } catch (error) {
-    console.error("エラー発生:", error);
-  }
-}
-
-</script>

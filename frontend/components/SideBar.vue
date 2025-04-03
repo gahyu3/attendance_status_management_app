0093fe031198a898844a3baa06c5becebed8e056
ishii hyuga <hyu825@gmail.com>
2025-04-04 00:11:54 +0900

    update: サイドバーをcomponentsに切り分け

---
+++ frontend/components/SideBar.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-navigation-drawer permanent>
+    <v-list-item title="Group"></v-list-item>
+    <v-divider></v-divider>
+    <v-list-item link title="List Item 1"></v-list-item>
+    <v-list-item link title="List Item 2"></v-list-item>
+    <v-list-item link title="List Item 3"></v-list-item>
+  </v-navigation-drawer>
+</template>
--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -1,15 +1,7 @@
 <template>
 
   <AppHeader />
-
-  <v-navigation-drawer permanent>
-    <v-list-item title="Group"></v-list-item>
-    <v-divider></v-divider>
-    <v-list-item link title="List Item 1"></v-list-item>
-    <v-list-item link title="List Item 2"></v-list-item>
-    <v-list-item link title="List Item 3"></v-list-item>
-  </v-navigation-drawer>
-
+  <SideBar />
   <v-main>
   </v-main>
 

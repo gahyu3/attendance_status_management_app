6a8f49a68c4d0c5acb169cc084dbbb6baa27422e
ishii hyuga <hyu825@gmail.com>
2025-04-04 18:58:00 +0900

    update: dashboardのindexアクションを修正

--- backend/app/controllers/api/v1/dashboards_controller.rb
+++ backend/app/controllers/api/v1/dashboards_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::DashboardsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
-    @group = Group.all
-    render json: { group: @group }
+    if params[:name].present?
+      group = Group.includes(attendants: :user).find_by(name: params[:name])
+
+      if group
+        render json: { group: group, group_attendants: group.attendants.as_json(include: :user) }
+      else
+        render json: { error: "Group not found" }, status: :not_found
+      end
+    end
+      groups = Group.all
+      render json: { groups: groups }
   end
 
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable
   include DeviseTokenAuth::Concerns::User
 
+  has_many :groups, through: :group_members
+
   validates :user_name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end

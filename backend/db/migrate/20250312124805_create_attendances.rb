2af285030fec070cc442649cd524c9cfb59190df
ishii hyuga <hyu825@gmail.com>
2025-04-16 23:18:12 +0900

    update: カラムにデフォルト値を設定

--- backend/db/migrate/20250312124805_create_attendances.rb
+++ backend/db/migrate/20250312124805_create_attendances.rb
@@ -2,8 +2,8 @@ class CreateAttendances < ActiveRecord::Migration[7.2]
   def change
     create_table :attendances do |t|
       t.date :date,                     null: false
-      t.string :schedule
-      t.string :attendances_status
+      t.string :schedule,               :default => "出席予定"
+      t.string :attendances_status,      :default => "出席中"
       t.string :remarks
       t.references :user,               null: false, foreign_key: true
       t.references :group,              null: false, foreign_key: true

7a781afee04bb454a458efe02cae76523fc27cf4
ishii hyuga <hyu825@gmail.com>
2025-04-17 20:45:49 +0900

    add: attendance_controllerのdestroyメソット作成

--- backend/app/controllers/api/v1/attendances_controller.rb
+++ backend/app/controllers/api/v1/attendances_controller.rb
@@ -6,8 +6,18 @@ class Api::V1::AttendancesController < ApplicationController
       if attendance.save
         render json: { attendance: attendance }, status: :ok
       else
-        render json: { error: attendance.errors.full_messages }
+        render json: { error: attendance.errors.full_messages }, status: :bad_request
       end
+    end
+
+    def destroy
+      attendance = Attendance.find(params[:id])
+      attendance.destroy!
+      render json: { message: "削除に成功しました" }, status: :ok
+    rescue ActiveRecord::RecordNotFound
+      render json: { message: "出席データが見つかりません" }, status: :not_found
+    rescue ActiveRecord::RecordNotDestroyed
+      render json: { message: "削除に失敗しました" }, status: :unprocessable_entity
     end
 
     private

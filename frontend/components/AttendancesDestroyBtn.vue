642a8fcb66fca5909d7c9a19924630b28cfdac8b
ishii hyuga <hyu825@gmail.com>
2025-04-25 19:51:42 +0900

    add: 出席状況を削除のボタンを追加

--- frontend/components/AttendancesDestroyBtn.vue
+++ frontend/components/AttendancesDestroyBtn.vue
@@ -1,6 +1,5 @@
 <template>
-  <v-btn>
-    削除
+  <v-btn icon="mdi mdi-close-thick" variant="tonal" size="x-small" color="red" @click="deleteAttendance(item.id)">
   </v-btn>
 </template>
 
@@ -12,6 +11,22 @@ defineProps({
   item:{}
 })
 
+const groupUserAttendancesData = useState("groupUserAttendancesData");
 
+const { getData: destroyAttendanceData,
+  postFetch: destroyAttendanceFetch
+} = usePostFetch(`${config.public.apiBase}/api/v1/attendances`);
+
+// 出席状況の削除
+async function deleteAttendance(attendanceId) {
+  await destroyAttendanceFetch("DELETE", attendanceId)
+
+  if (destroyAttendanceData.value) {
+    const index = groupUserAttendancesData.value.findIndex(a => a.id === attendanceId)
+    groupUserAttendancesData.value.splice(index, attendanceId)
+  } else {
+    console.log("データがありません")
+  }
+}
 
 </script>

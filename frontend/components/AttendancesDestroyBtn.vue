c95eb550711313363a33657630b05554cff75703
ishii hyuga <hyu825@gmail.com>
2025-04-25 18:50:55 +0900

    add: 削除ボタンをログインしているユーザーだけ表示

---
+++ frontend/components/AttendancesDestroyBtn.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn>
+    削除
+  </v-btn>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const config = useRuntimeConfig()
+
+defineProps({
+  item:{}
+})
+
+
+
+</script>

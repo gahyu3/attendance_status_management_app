36618893a21f488beb34bbceab80c1c18f469e7c
ishii hyuga <hyu825@gmail.com>
2025-04-27 18:05:12 +0900

    add: プロフィール編集のapiを作成

---
+++ backend/app/controllers/api/v1/profiles_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    render json: current_user, status: :ok
+  end
+
+  def update
+    if current_user.update(profile_params)
+      render json: current_user, status: :ok
+    else
+      render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:user).permit(:user_name)
+  end
+
+end

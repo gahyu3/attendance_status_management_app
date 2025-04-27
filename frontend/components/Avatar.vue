5cc97bcf4fa1f93783976a2666ec2f829d8b3d44
ishii hyuga <hyu825@gmail.com>
2025-04-27 15:19:46 +0900

    add: アバター画像を表示

---
+++ frontend/components/Avatar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-avatar v-if="userImageUrl"
+            :image="`${config.public.apiBase}${userImageUrl}`"
+            size="32"
+            start/>
+  <v-icon v-else icon="mdi-account-circle" size="x-large" start/>
+</template>
+
+<script setup>
+defineProps({
+  userImageUrl: ""
+})
+const config = useRuntimeConfig();
+
+</script>

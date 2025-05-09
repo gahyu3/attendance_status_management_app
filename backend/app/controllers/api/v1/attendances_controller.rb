6b52205d4f07fd57f9786e33b881f5fac79888b3
ishii hyuga <hyu825@gmail.com>
2025-05-09 20:21:16 +0900

    update: 出席一覧をattendance_controllerに移動

--- backend/app/controllers/api/v1/attendances_controller.rb
+++ backend/app/controllers/api/v1/attendances_controller.rb
@@ -1,6 +1,32 @@
 class Api::V1::AttendancesController < ApplicationController
     before_action :authenticate_user!
 
+    def index
+      if params[:name].present? && params[:date].present?
+        group = Group.find_by(name: params[:name])
+        group_user_attendances = group.attendances.includes(:user).where(date: params[:date])
+
+        if group
+          result = group_user_attendances.map do |attendance|
+            attendance.as_json(
+              include: { user: { only: [:id, :user_name, :avatar_image] } },
+              only: [:id, :date, :remarks, :user_id, :group_id]
+            ).merge(
+              schedule: attendance.schedule_i18n,
+              attendances_status: attendance.attendances_status_i18n
+            )
+          end
+
+          render json: { attendances: result }
+
+        else
+          render json: { error: "Group not found" }, status: :not_found
+        end
+      else
+        render json: { error: "Name and date must be provided" }, status: :bad_request
+      end
+    end
+
     def create
       attendance = Attendance.new(attendance_params)
       if attendance.save

47b759894f67ab178b92d0fca34431f3363abcbc
ishii hyuga <hyu825@gmail.com>
2025-05-11 19:14:17 +0900

    add: 参加予定の編集ダイアログを作成

--- frontend/components/AttendancesScheduleBtn.vue
+++ frontend/components/AttendancesScheduleBtn.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-btn>{{ scheduleToJapanese(item.schedule) }}</v-btn>
+  <v-btn @click="dialog = true" :color="btnColor(item.schedule)">{{ scheduleToJapanese(item.schedule) }}</v-btn>
+  <ScheduleModal v-model="dialog" :item="item" />
 </template>
 
 <script setup>
@@ -10,11 +11,25 @@ defineProps({
   item:{}
 })
 
+const dialog = ref(false)
+
 const scheduleMap = {
   full_day_attendance: "終日参加",
   morning_attendance: "午前参加",
   afternoon_attendance: "午後参加"
 }
+
+const scheduleColorMap = {
+  full_day_attendance: "success",
+  morning_attendance: "yellow",
+  afternoon_attendance: "grey"
+}
+
+// ステータスに応じたボタンの色を返す
+function btnColor(scheduleStatus) {
+  return scheduleColorMap[scheduleStatus] || ""
+}
+
 
 // ステータスを日本語化
 function scheduleToJapanese(schedule) {
---
+++ frontend/components/ScheduleModal.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-dialog v-model="dialog"
+            width="auto">
+    <v-card width="500"
+            style="max-width: 90vw;"
+            title="予定編集"
+            class="pa-10">
+      <v-form>
+        <div class="py-5">
+          <v-chip size="x-large" color="primary" variant="tonal">
+            {{ formatDate }}
+          </v-chip>
+        </div>
+        <v-select
+          v-model="item.schedule"
+          label="参加予定"
+          :items="scheduleList"
+          variant="outlined"
+        ></v-select>
+        <v-text-field v-model="item.remarks"
+                        label="備考"
+                        type="text"
+                        variant="outlined"
+                        maxlength="10"
+                        :counter="10"
+                        ></v-text-field>
+        <v-btn type="submit">
+          編集
+        </v-btn>
+      </v-form>
+      <v-btn class="ms-auto"
+            text="閉じる"
+            @click="dialog = false">
+      </v-btn>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+defineProps({
+  item:{}
+})
+
+const dialog = defineModel()
+const { selectedDate, formatDate } = useDatePicker()
+const scheduleList = [
+  { title: "終日参加", value: "full_day_attendance"},
+  { title: "午前参加", value: "morning_attendance" },
+  { title: "午後参加", value: "afternoon_attendance" },
+];
+
+</script>

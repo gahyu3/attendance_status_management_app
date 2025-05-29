a749707e4dd4efd5980072860ff1aa7b66940f1d
ishii hyuga <hyu825@gmail.com>
2025-05-29 18:42:52 +0900

    add: group_controllerのテストを追加

--- backend/spec/factories/attendances.rb
+++ backend/spec/factories/attendances.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :attendance do
-    
+    sequence(:date) { |n| Date.today + n.days }
+    schedule { Attendance.schedules.keys.sample }
+    attendances_status { Attendance.attendances_statuses.keys.sample }
+    remarks { Faker::Lorem.sentence }
+
+    association :user
+    association :group
   end
 end

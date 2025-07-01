913a08480ca09b32d996994ee716f7e77d7ea79d
ishii hyuga <hyu825@gmail.com>
2025-07-02 05:39:18 +0900

    add: カレンダーのfeatchを実装

--- frontend/pages/calendar.vue
+++ frontend/pages/calendar.vue
@@ -2,7 +2,6 @@
   <NuxtLink to="/dashboard">
     <v-icon icon="mdi mdi-calendar-blank-outline"></v-icon>
   </NuxtLink>
-  <v-btn @click="changeAttendance(attendanceList)">aaa</v-btn>
   <FullCalendar :options="calendarOptions" />
 </template>
 
@@ -10,7 +9,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { useDate } from 'vuetify'
-import type { Attendance, EventItem, Schedule } from '~/types';
+import type { Attendance, AttendancesResponse, EventItem, Schedule } from '~/types';
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
@@ -21,54 +20,31 @@ definePageMeta({
     "auth",
   ],
 });
+const config = useRuntimeConfig();
+const { getAuthHeaders } = useApiClient()
 
 const { selectedDate } = useDatePicker()
 
-const attendanceList = [{
-  id: 10,
-  date: "2025-06-13",
-  schedule: "full_day_attendance",
-  attendances_status: "present",
-  remarks: "",
-  user_id: 7,
-  group_id: 2,
-  created_at: "2025-05-29T18:47:24.379+09:00",
-  updated_at: "2025-05-29T18:47:24.379+09:00",
-  user: {
-    id: 7,
-    provider: "email",
-    uid: "brittney_brekke@collins-kling.test",
-    allow_password_change: false,
-    user_name: "野村 明日香",
-    email: "brittney_brekke@collins-kling.test",
-    avatar_image: {
-      url: "/uploads/user/avatar_image/7/cat.jpg"
-    }
-  }},
-{
-  id: 10,
-  date: "2025-06-20",
-  schedule: "full_day_attendance",
-  attendances_status: "present",
-  remarks: "",
-  user_id: 7,
-  group_id: 2,
-  created_at: "2025-05-29T18:47:24.379+09:00",
-  updated_at: "2025-05-29T18:47:24.379+09:00",
-  user: {
-    id: 7,
-    provider: "email",
-    uid: "brittney_brekke@collins-kling.test",
-    allow_password_change: false,
-    user_name: "野村 明日香",
-    email: "brittney_brekke@collins-kling.test",
-    avatar_image: {
-      url: "/uploads/user/avatar_image/7/cat.jpg"
-    },
-  }
-}]
-
 const events: Ref<EventItem[]> = ref([]);
+
+async function getCalendar(date: string, userId: number, groupId: number): Promise<void> {
+  try {
+    const response: AttendancesResponse = await $fetch(`${config.public.apiBase}/api/v1/attendances/calendar`, {
+      headers: getAuthHeaders(),
+      query: {
+        date: date,
+        user_id: userId,
+        group_id: groupId
+      }
+    })
+
+    changeAttendance(response)
+
+  } catch (error) {
+    console.error('APIエラー', error)
+  }
+}
+
 
 function changeAttendance(value: any) {
   value.forEach((item: Attendance) => {
